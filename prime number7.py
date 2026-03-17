ccc9a97887a5797b97004ec03271becd18e875f8
mshmsh6980-byte <mshmsh6980@gmail.com>
2026-03-17 21:56:00 +0300

    end commit

--- prime number7.py
+++ prime number7.py
@@ -1,11 +1,9 @@
 x=int(input("enter the number:"))
-primefound=True
-if x>1:
-    for i in range(2,x):
-        if (x%i)==0:
-            primefound=False
-            break
-if primefound:
-    print(x,"is a prime number")
-else:
-    print(x,"is not a prime number")
+for i in range(2,x-1):
+    if (x%i)==0:
+        print(x, "is not a prime number")
+        break
+    else:
+         print(x, "is  a prime number")
+
+
